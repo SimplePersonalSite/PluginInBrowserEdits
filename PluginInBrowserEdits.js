e6b501ee13de4cb190a8abb929e0ec446a56d504
Josh Terrell <joshterrell805@gmail.com>
2016-12-28 09:37:58 -0800

    Edit in place

--- PluginInBrowserEdits.js
+++ PluginInBrowserEdits.js
@@ -1,75 +1,71 @@
 var PluginInBrowserEdits = Util.createSingleton('PluginInBrowserEdits', function() {
-  this.fileEditModal = new PluginInBrowserEdits.FileEditModal();
+  this._editing = false;
+  this._saved = false;
 });
 PluginInBrowserEdits.plugin = function plugin() {
   PluginInBrowserEdits.getInstance().init();
   SimplePersonalSite.Util.linkCss('PluginInBrowserEdits.css');
-  return SimplePersonalSite.Util.pLinkJs(
-      'https://cdn.rawgit.com/Nycto/PicoModal/master/src/picoModal.js');
 };
 PluginInBrowserEdits.port = 4113;
+PluginInBrowserEdits.autoGrow = function autoGrow(elm) {
+  // http://stackoverflow.com/a/24676492/1433127
+  elm.style.height = "auto";
+  elm.style.height = (elm.scrollHeight)+"px";
+};
 PluginInBrowserEdits.prototype.init = function init() {
   var old_render = SimplePersonalSite.Context.prototype.render_md_file;
   var new_render = function render_md() {
     var html = old_render.apply(this, arguments);
     var filename = arguments[0];
-    return '<div class="sps-ibe-editable-markup" data-filename="' + filename + '"><div ' + 
-        'onclick="PluginInBrowserEdits.getInstance().clickEdit(event)" class="sps-ibe-edit-btn">' +
-        filename + '</div>' + html + '</div>';
+    return '<div class="sps-ibe-editable-markup" data-filename="' + filename + '" ' +
+        'ondblclick="PluginInBrowserEdits.getInstance().clickEdit(event)"><div ' +
+        'onclick="PluginInBrowserEdits.getInstance().clickEdit(event)" ' +
+        'class="sps-ibe-edit-btn">' + filename + '</div>' + html + '</div>';
   };
   SimplePersonalSite.Context.prototype.render_md_file = new_render;
 };
 PluginInBrowserEdits.prototype.clickEdit = function clickEdit(evt) {
-  var filename = this.getFilename(evt);
-  this.fileEditModal.pShow(filename).catch(console.error.bind(console));
-};
-PluginInBrowserEdits.prototype.getFilename = function getFilename(evt) {
-  return evt.target.parentNode.dataset.filename;
-};
-
-PluginInBrowserEdits.FileEditModal = function FileEditModal() {
-  this._modal = null;
-  this._saved = false;
-  this._p = null;
-  this._filename = null;
-};
-PluginInBrowserEdits.FileEditModal.prototype.pShow = function pShow(filename) {
-  var self = this;
-  return SimplePersonalSite.Util.pFetch(filename)
-    .then(function(md) {
-      if (self._modal !== null) {
-        return Promise.reject(new Error('Cannot show modal when another modal is up'));
-      }
-      self._filename = filename;
-      self._saved = false;
-      self._modal = picoModal('<textarea id="sps-ibe-file-text">' + md + '</textarea>');
-      self._modal.show();
-      self._p = new SimplePersonalSite.Util.ResolvablePromise();
-      self._modal.beforeClose(self.beforeClose.bind(self));
-      return self._p;
-    });
-};
-PluginInBrowserEdits.FileEditModal.prototype.beforeClose = function beforeClose() {
-  var self = this;
-  try {
-    if (self._saved) {
-      return;
-    }
-    var elm = document.getElementById('sps-ibe-file-text');
-    var content = elm.value;
-    var msg = JSON.stringify({'filename': self._filename, 'content': content});
-    var url = window.location.protocol + '//' + window.location.hostname + ':' +
-        PluginInBrowserEdits.port;
-    SimplePersonalSite.Util.pAjax(url, 'post', msg)
-      .then(function() {
-        self._saved = true;
-        self._modal.close();
-        self._modal = null;
-        SimplePersonalSite.App.getInstance().refresh();
-      })
-      .then(self._p.resolve)
-      .catch(self._p.reject);
-  } catch (e) {
-    self._p.reject(e);
+  if (this._editing) {
+    return alert('already editing');
   }
+  this._editing = true;
+  var container = evt.target;
+  while (container !== null && !container.classList.contains('sps-ibe-editable-markup')) {
+    container = container.parentNode;
+  }
+  SimplePersonalSite.Util.assert(container !== null);
+  var filename = container.dataset.filename;
+
+  var self = this;
+  SimplePersonalSite.Util.pFetch(filename)
+      .then(function(md) {
+        container.innerHTML =
+            '<div class="sps-ibe-save-btn">save</div><textarea ' +
+            'onblur="PluginInBrowserEdits.getInstance().clickSave()" ' +
+            'oninput="PluginInBrowserEdits.autoGrow(this)" id="sps-ibe-file-text">' +
+            md + '</textarea>';
+        container.childNodes[1].focus();
+        PluginInBrowserEdits.autoGrow(container.childNodes[1]);
+        self._saved = false;
+      })
+      .catch(console.error.bind(console));
+};
+PluginInBrowserEdits.prototype.clickSave = function clickSave(evt) {
+  if (this._saved) {
+    return;
+  }
+  this._saved = true;
+  var elm = document.getElementById('sps-ibe-file-text');
+  var content = elm.value;
+  var msg = JSON.stringify({'filename': elm.parentNode.dataset.filename,
+      'content': content});
+  var url = window.location.protocol + '//' + window.location.hostname + ':' +
+      PluginInBrowserEdits.port;
+  var self = this;
+  SimplePersonalSite.Util.pAjax(url, 'post', msg)
+    .then(function() {
+      SimplePersonalSite.App.getInstance().refresh();
+      self._editing = false;
+    })
+    .catch(console.error.bind(console));
 };
